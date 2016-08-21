5689d538bbaa5b8fde7ffc236b80c2d84ea4421d
Joss Smith <git@obadonke.com>
2016-08-21 20:14:15 +0100

    Added 'middleware' all method

--- src/routes/books.js
+++ src/routes/books.js
@@ -14,58 +14,63 @@ var emptyBook = {
 
 var routerFactory = function (navItems) {
 
-bookRouter.route('/')
-    .get(function (req, res) {
-        var request = new sql.Request();
+    bookRouter.route('/')
+        .get(function (req, res) {
+            var request = new sql.Request();
 
-        request.query('select * from books', function (err, recordset) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            res.render('books', {
-                title: 'Fledgling books',
-                nav: navItems,
-                books: recordset
+            request.query('select * from books', function (err, recordset) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                res.render('books', {
+                    title: 'Fledgling books',
+                    nav: navItems,
+                    books: recordset
+                });
             });
+
         });
 
-    });
 
-bookRouter.route('/:id')
-    .get(function (req, res) {
+    bookRouter.route('/:id')
+        .all(function (req, res, next) {
+            var bookRenderCallback = function (err, recordset) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
 
-        var renderCallback = function (err, recordset) {
-            if (err) {
-                console.log(err);
-                return;
-            }
+                if (recordset.length == 0) {
+                    recordset = [emptyBook];
+                }
 
-            if (recordset.length == 0) {
-                recordset = [emptyBook];
-            }
+                req.book = recordset[0];
+                next();
+            };
 
+            var ps = new sql.PreparedStatement();
+            ps.input('id', sql.Int);
+            ps.prepare('select * from books where id=@id', function (err) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                ps.execute({
+                    id: req.params.id
+                }, bookRenderCallback);
+            });
+        })
+        .get(function (req, res) {
             res.render('book', {
                 title: 'Fledgling books',
                 nav: navItems,
-                book: recordset[0]
+                book: req.book
             });
-        };
 
-        var ps = new sql.PreparedStatement();
-        ps.input('id', sql.Int);
-        ps.prepare('select * from books where id=@id', function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            ps.execute({
-                id: req.params.id
-            }, renderCallback);
         });
-    });
 
-return bookRouter;
+    return bookRouter;
 };
 
 
