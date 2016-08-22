afd65385e51a2461fe03d6c73c12e0e85c1eee0c
obadonke <git@obadonke.com>
2016-08-22 20:43:56 +0100

    Added admin routes to insert and drop books from database

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 "use strict";
 
 var express = require('express');
-
 var app = express();
 
 var port = process.env.PORT || 5000;
 
 var navItems = [
-    {
-        link: '/books',
-        text: 'Bookz'
+   {
+      link: '/books',
+      text: 'Bookz'
     }, {
-        link: '/authors',
-        text: 'Authorz'
+      link: '/authors',
+      text: 'Authorz'
     }];
 
+var mongodb = require('mongodb');
+var mongoConnect = function (callback) {
+   var url = 'mongodb://localhost:27017/nodeExpressLibraryApp';
+
+   mongodb.connect(url, function (err, db) {
+      if (err) {
+         console.log(err);
+         return;
+      }
+
+      callback(db);
+   });
+};
+
 var bookRouter = require('./src/routes/books')(navItems);
+
+var adminRouter = require('./src/routes/admin')(mongoConnect);
+
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -23,14 +39,15 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.listen(port, function () {
-    console.log(`running server on port ${port}`);
+   console.log(`running server on port ${port}`);
 });
 
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Fledgling books',
-        nav: navItems
-    });
+   res.render('index', {
+      title: 'Fledgling books',
+      nav: navItems
+   });
 });
---
+++ src/routes/admin.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var adminRouter = express.Router();
+
+var theBooks = require('./hardcodedBooks');
+
+var router = function (mongoConnect) {
+   adminRouter.route('/addBooks')
+      .get(function (req, res) {
+         mongoConnect(function (db) {
+            var collection = db.collection('books');
+            collection.insertMany(theBooks, function (err, results) {
+               if (err) {
+                  console.log(err);
+                  return;
+               }
+               res.send(results);
+               db.close();
+            });
+         });
+      });
+
+   adminRouter.route('/dropBooks')
+      .get(function (req, res) {
+         mongoConnect(function (db) {
+            var collection = db.collection('books');
+            collection.drop(function (err /*, reply*/ ) {
+               if (err) {
+                  console.log(err);
+                  return;
+               }
+               res.send("We deleted the collection");
+               db.close();
+            });
+         });
+      });
+
+   return adminRouter;
+};
+
+module.exports = router;
